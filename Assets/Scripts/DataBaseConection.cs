43fd5ffd155a8615d9fa6538e509b781a9135d47
frijol252 <josegamer252@gmail.com>
2020-11-24 17:26:54 -0400

    Primera coneccion con el repositorio Clever

---
+++ Assets/Scripts/DataBaseConection.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+public class DataBaseConection : MonoBehaviour
+{
+    void Start()
+    {
+        LLamarBase();
+    }
+    void LLamarBase()
+    {
+        MySqlConnection conn = new MySqlConnection();
+        string cadenaConn = "Server=bojvq5dmatxn6x6sqhq7-mysql.services.clever-cloud.com;DataBase=bojvq5dmatxn6x6sqhq7;Uid=ubdyhzvbbvfhkb83;Pwd=PUXrlBXnY5HYAgaY938E;";
+        conn.ConnectionString = cadenaConn;
+        try
+        {
+            conn.Open();
+            Debug.Log("Conexion Establecida");
+        }
+        catch(MySqlException ex)
+        {
+            Debug.Log("Conexion No Establecida"+ex);
+        }
+    }
+}
