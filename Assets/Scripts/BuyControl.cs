9813b6f53ce4c6ed1eefcde4474172412568ea7e
mirko23019 <mmmiko23@gmail.com>
2020-11-30 11:31:18 -0400

    BuyStories

---
+++ Assets/Scripts/BuyControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyControl : MonoBehaviour
+{
+    public Button btnBuy;
+    public Button btnHistoria;
+    public Text msg;
+    public void successBuy(){
+        btnBuy.transform.localScale = new Vector3(0, 0, 0);
+        msg.text="Compra exitosa";
+        StartCoroutine("mrgOut");
+        btnHistoria.enabled=true;
+    }
+    public void failBuy(){
+        msg.text="Compra fallida";
+        StartCoroutine("mrgOut");
+    }
+    IEnumerator mrgOut(){
+        yield return new WaitForSeconds(2);
+        msg.text="";
+    }
+}
---
+++ Assets/Scripts/Desable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Desable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().enabled=false;
+    }
+}
