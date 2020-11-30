220f897be096aec0df699a152d4bdbd5de57ba9c
mirko23019 <mmmiko23@gmail.com>
2020-11-30 12:59:22 -0400

    Sprint 04

---
+++ Assets/Scripts/MovimientoTutorial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovimientoTutorial : MonoBehaviour
+{
+    public float speed = 4f;
+    Rigidbody2D rb2d;
+    Vector2 mov;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mov=new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        );
+    }
+    public void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag=="enemigoTuto"){
+            SceneManager.LoadScene(6);
+        }
+    }
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+    }
+}
